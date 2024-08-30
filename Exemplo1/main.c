34a541454ac1b6f91506b334603543ad458cea08
Tauã Felipe <127506090+felipetaua@users.noreply.github.com>
2024-08-30 07:30:53 -0300

    Add files via upload

---
+++ Exemplo1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Este é o primeiro exemplo do curso de linguagem C
+int main(void) {
+  // Exibe as informações no console
+  printf("Hello World\n");
+  printf("Meu primeiro trabalho em C\n");
+  printf("Meu nome é: Tauã Felipe\n");
+  // Sucesso ao compilar o código
+  return 0;
+}
